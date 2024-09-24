cced48345ab59bb8ed369939344e77d20d8a91e9
aarostami <alireza.ro76@gmail.com>
2024-09-24 19:58:32 +0330

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -2,10 +2,10 @@
 const nextConfig = {
 	//basePath: '/project23-theme3',	//baraye local bayad ino comment konim
 	//output: 'export',
-	/* images: {
-		path: "/",
+	images: {
+		// path: "/",
 		unoptimized: true
-	} */
+	},
 	images: {
 		unoptimized: true
 	}
