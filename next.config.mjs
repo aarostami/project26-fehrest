5cd21cedd2415f562ea0cacb61f87c0fc62cbcce
aarostami <alireza.ro76@gmail.com>
2024-09-28 19:57:13 +0330

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-	basePath: '/project26-fehrest',	//baraye local bayad ino comment konim
+	//basePath: '/project26-fehrest',	//baraye local bayad ino comment konim
 	//output: 'export',
 	images: {
 		// path: "/",
